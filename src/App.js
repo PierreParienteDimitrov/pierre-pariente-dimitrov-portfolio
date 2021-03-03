c44d21e845e78971af3f20576abaa6ddeab2e647
Pierre Pariente <pierre.parientedimitrov@gmail.com>
2021-03-03 17:09:45 +0100

    created basic layout and implemented react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import React from 'react';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import Home from './Pages/Home';
+import Contact from './Pages/Contact';
+
 import './App.scss';
+import Navigation from './Layout/Navigation';
+import Footer from './Layout/Footer';
 
 function App() {
 	return (
-		<div className='App'>
-			<p>Hello</p>
-		</div>
+		<Router>
+			<div className='App'>
+				<Navigation />
+				<Route exact path='/' component={Home} />
+				<Route path='/contact' component={Contact} />
+				<Footer />
+			</div>
+		</Router>
 	);
 }
 
