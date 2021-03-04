8586c444828da8023715a6a250ff37c27619eb8c
Pierre Pariente <pierre.parientedimitrov@gmail.com>
2021-03-04 22:36:32 +0100

    added hero section on homepage

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -3,10 +3,37 @@ import './_hero.scss';
 
 const Hero = () => {
 	return (
-		<header className='hero'>
-			<div>This is the big texts</div>
-			<p>this is the small description</p>
-		</header>
+		<>
+			<header id='showcase'>
+				<div className='content-wrap hero-grid'>
+					<div className='list'>
+						<ul>
+							<li>
+								<a href='/code'>product designer</a>
+							</li>
+							<li>
+								<a href='/design'>
+									<span className='link-italic'>developer</span>
+								</a>
+							</li>
+							<li>
+								<a href='/entrepreneur'>entrepreneur</a>
+							</li>
+						</ul>
+					</div>
+
+					<div className='description'>
+						<p>
+							Hi, my name is <strong>Pierre Pariente Dimitrov</strong>. I create
+							websites and applications.
+						</p>
+						<a href='/contact' className='contact'>
+							CONTACT ME
+						</a>
+					</div>
+				</div>
+			</header>
+		</>
 	);
 };
 
