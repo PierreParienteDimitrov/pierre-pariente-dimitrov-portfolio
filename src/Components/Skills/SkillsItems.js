cc49490a698b243e7dffe782c9f8dd3470b53e82
Pierre Pariente <pierre.parientedimitrov@gmail.com>
2021-03-06 12:34:54 +0100

    added new component for skills

---
+++ src/Components/Skills/SkillsItems.js
@@ -0,0 +1,17 @@
+export const SkillsItems = [
+	{
+		number: '01.',
+		title: 'designer',
+		description: 'This is the description',
+	},
+	{
+		number: '02.',
+		title: 'developer',
+		description: 'This is the description',
+	},
+	{
+		number: '03.',
+		title: 'entrepreneur',
+		description: 'This is the description',
+	},
+];
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import Card from '../../Components/Card/Card';
 import Hero from '../../Components/Hero/Hero';
+import Skills from '../../Components/Skills/Skills';
 import './_home.scss';
 
 const Home = () => {
 	return (
-		<div className='main'>
-			<Hero />
-			<Card />
-		</div>
+		<>
+			<div className='main'>
+				<Hero />
+			</div>
+			<div className='w-full h-full py-40 skills-bg-color'>
+				<div className='main'>
+					<Skills />
+				</div>
+			</div>
+
+			<div className='main'>
+				<Card />
+			</div>
+		</>
 	);
 };
 
