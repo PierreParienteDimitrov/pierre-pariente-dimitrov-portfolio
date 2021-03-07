397ee7c839a0dbdb508ff74217592344b40d0cf6
Pierre Pariente <pierre.parientedimitrov@gmail.com>
2021-03-07 17:42:00 +0100

    deploy

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,7 +3,6 @@ import Hero from './Hero/Hero';
 import Container from '../../Layout/Container/Container';
 import ContainerFluid from '../../Layout/ContainerFluid/ContainerFluid';
 import Skills from './Skills/Skills';
-import BtnPrimary from '../../Components/BtnPrimary/BtnPrimary';
 import './_home.scss';
 import CardsGrid from './CardsGrid/CardsGrid';
 
